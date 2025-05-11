f17e6a5e938d80491cd5aa8dab611c4bfe2276d9
Ronanko <150694954+R0nank0@users.noreply.github.com>
2025-05-11 17:16:38 +0200

    Update .eleventy.js

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,13 @@
+const { DateTime } = require("luxon");
+
 module.exports = function(eleventyConfig) {
   eleventyConfig.addPassthroughCopy("admin");
   eleventyConfig.addPassthroughCopy("images");
+
+  // Ajout du filtre "date"
+  eleventyConfig.addFilter("date", (value, format = "dd/MM/yyyy") => {
+    return DateTime.fromJSDate(new Date(value)).toFormat(format);
+  });
 
   return {
     dir: {
